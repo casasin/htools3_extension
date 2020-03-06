6da8550fed2ea6eaa50368f5270310689e3159c2
Gustavo Ferreira <gustavo@hipertipo.com>
2020-03-06 17:08:09 -0300

    switching preferences window to accordion, bump version number to 0.4.5

--- hTools3.roboFontExt/lib/hTools3/dialogs/preferences.py
+++ hTools3.roboFontExt/lib/hTools3/dialogs/preferences.py
@@ -2,10 +2,11 @@ from importlib import reload
 import hTools3.dialogs
 reload(hTools3.dialogs)
 
-from vanilla import TextBox, ColorWell, Slider, CheckBox
-from mojo.UI import UpdateCurrentGlyphView
+from vanilla import TextBox, ColorWell, Slider, CheckBox, Group
+from mojo.UI import UpdateCurrentGlyphView, AccordionView
 from hTools3.dialogs import hDialog
 from hTools3.modules.color import rgbToNSColor, nsColorToRGB
+
 
 class PreferencesDialog(hDialog):
 
@@ -19,48 +20,68 @@ class PreferencesDialog(hDialog):
 
     '''
 
-    title = 'settings'
+    title = 'preferences'
     key = '%s.preferences' % hDialog.key
-    windowType = 1
+    windowType = 0
 
     def __init__(self):
-        self.height  = self.buttonHeight * 2
-        self.height += self.textHeight * 5
-        self.height += self.padding * 5 - 3
-        self.w = self.window((self.width, self.height), self.title)
+        self.width  *= 1.2
+        self.height = 360
+        self.w = self.window(
+                (self.width, self.height), self.title,
+                minSize=(self.width, self.height * 0.75),
+                maxSize=(self.width * 1.5, self.height * 2))
+
+        self.initPreviewGroup()
+        self.initOptionsGroup()
+        self.initAccordionView()
+
+        self.w.open()
+
+    # -------------
+    # dynamic attrs
+    # -------------
+
+    # ------------
+    # initializers
+    # ------------
+
+    def initPreviewGroup(self):
+
+        self.previewGroup =  Group((0, 0, -0, -0))
 
         x = y = p = self.padding
-        self.w.previewFillColorLabel = TextBox(
+        self.previewGroup.previewFillColorLabel = TextBox(
                 (x, y, -p, self.textHeight),
                 'fill color',
                 sizeStyle=self.sizeStyle)
 
         y += self.textHeight
-        self.w.previewFillColor = ColorWell(
+        self.previewGroup.previewFillColor = ColorWell(
                 (x, y, -p, self.buttonHeight),
                 color=rgbToNSColor(self.previewFillColor),
                 callback=self.savePreviewFillColorCallback)
 
         y += self.buttonHeight + p
-        self.w.previewStrokeColorLabel = TextBox(
+        self.previewGroup.previewStrokeColorLabel = TextBox(
                 (x, y, -p, self.textHeight),
                 'stroke color',
                 sizeStyle=self.sizeStyle)
 
         y += self.textHeight
-        self.w.previewStrokeColor = ColorWell(
+        self.previewGroup.previewStrokeColor = ColorWell(
                 (x, y, -p, self.buttonHeight),
                 color=rgbToNSColor(self.previewStrokeColor),
                 callback=self.saveStrokeColorCallback)
 
         y += self.buttonHeight + p
-        self.w.previewStrokeWidthLabel = TextBox(
+        self.previewGroup.previewStrokeWidthLabel = TextBox(
                 (x, y, -p, self.textHeight),
                 'stroke width',
                 sizeStyle=self.sizeStyle)
 
         y += self.textHeight
-        self.w.previewStrokeWidth = Slider(
+        self.previewGroup.previewStrokeWidth = Slider(
                 (x, y, -p, self.textHeight),
                 value=self.previewStrokeWidth,
                 minValue=0,
@@ -71,20 +92,37 @@ class PreferencesDialog(hDialog):
                 sizeStyle=self.sizeStyle)
 
         y += self.textHeight + p
-        self.w.verbose = CheckBox(
+        self.previewGroupHeight = y
+
+    def initOptionsGroup(self):
+
+        self.optionsGroup =  Group((0, 0, -0, -0))
+
+        x = y = p = self.padding
+        self.optionsGroup.verbose = CheckBox(
                 (x, y, -p, self.textHeight),
                 "verbose",
                 value=self.verbose,
                 callback=self.saveVerboseCallback,
                 sizeStyle=self.sizeStyle)
 
-        self.w.open()
+        y += self.textHeight + p
+        self.optionsGroupHeight = y
 
-    # -------------
-    # dynamic attrs
-    # -------------
-
-    # ...
+    def initAccordionView(self):
+        descriptions = [
+            dict(label="preview",
+                view=self.previewGroup,
+                size=self.previewGroupHeight,
+                collapsed=False,
+                canResize=False),
+            dict(label="options",
+                view=self.optionsGroup,
+                size=self.optionsGroupHeight,
+                collapsed=False,
+                canResize=False),
+        ]
+        self.w.accordionView = AccordionView((0, 0, -0, -0), descriptions)
 
     # ---------
     # callbacks
