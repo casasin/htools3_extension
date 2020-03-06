58e03f9ae576eb9cf6d5e4ef9b6f4ef0ef0e833a
Gustavo Ferreira <gustavo@hipertipo.com>
2020-03-06 14:17:28 -0300

    maintenance and cleanup

--- hTools3.roboFontExt/lib/start.py
+++ hTools3.roboFontExt/lib/start.py
@@ -1,39 +1,15 @@
 import os
 import sys
 
-def add_hTools3_module(verbose=False):
-    libFolder = os.getcwd()
-    if verbose:
-        print('\tadding hTools3 module to sys.path...')
-    sys.path.append(libFolder)
+# add module
+libFolder = os.getcwd()
+print('\tadding hTools3 module to sys.path...')
+sys.path.append(libFolder)
 
-def add_hTools3_menu_main(folderName, verbose=False):
-    from hTools3.modules.sys import addMenu
-    folderName = os.path.join(os.getcwd(), folderName)
-    if verbose:
-        print('\tadding hTools3 to the main application menu...')
-    addMenu('hTools3', folderName)
-
-def add_hTools3_menu_contextual_font(folderName, verbose=False):
-    if verbose:
-        print('\tadding hTools3 to the Font Overview contextual menu...')
-    pass
-
-# --------
-# settings
-# --------
-
-VERBOSE = True
-DEVMODE = False
-
-# ----------
-# initialize
-# ----------
-
-menusFolder = 'menus' if not DEVMODE else 'menus_dev'
-
+# add menu
+from hTools3.modules.sys import add_hTools3_module, add_hTools3_menu_main
 print('initializing hTools3...\n')
-add_hTools3_module(verbose=VERBOSE)
-add_hTools3_menu_main(menusFolder, verbose=VERBOSE)
+add_hTools3_module(verbose=True)
+add_hTools3_menu_main('menus', verbose=True)
 # add_hTools3_menu_contextual_font(menusFolder, verbose=VERBOSE)
 print('\n...done.\n')
